bc30764bb6b3e8d543c51b39fe73c2911dafcc4a
gabrieleculotta <culotta.gabriele@gmail.com>
2021-10-28 19:32:58 +0200

    init

---
+++ webscarping.js
@@ -0,0 +1,32 @@
+const PORT = 8000;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+const app = express();
+
+const url = "https://www.spaziogames.it/"; // -- insert here URL
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    //console.log(html);
+    const articles = [];
+    const $ = cheerio.load(html);
+    $(".post_template_standard_text", html).each(function () {
+      const title = $(this)
+        .find("span")
+        .text()
+        .trim("\n")
+        .replace(/ +(?= )/g, ""); //prendo det. parametri (regex??)
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
